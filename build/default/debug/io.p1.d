96ccde24eac2ec48787cfae297d246ca975b7d77
Rogue <jrsmatos97@gmail.com>
2022-02-21 14:50:22 +0000

    .

--- build/default/debug/io.p1.d
+++ build/default/debug/io.p1.d
@@ -1,8 +1,2 @@
-build/default/debug/io.p1: 
-io.h
-C:\Program Files (x86)\HI-TECH Software\PICC-18\9.80\include\errata.h
-C:\Program Files (x86)\HI-TECH Software\PICC-18\9.80\include\htc.h
-C:\Program Files (x86)\HI-TECH Software\PICC-18\9.80\include\pic18f26k22.h
-C:\Program Files (x86)\HI-TECH Software\PICC-18\9.80\include\chip_select.h
-C:\Program Files (x86)\HI-TECH Software\PICC-18\9.80\include\trace18.h
-C:\Program Files (x86)\HI-TECH Software\PICC-18\9.80\include\pic18.h
+build/default/debug/io.p1:	\
+	io.h	\
--- main.c
+++ main.c
@@ -43,14 +43,13 @@
 #define COMMAND_CHANGE_FISH_5   0b00111001     // Change fish command (Molly)
 #define COMMAND_CHANGE_FISH_6   0b01000001     // Change fish command (Neon)
 
-//#define Reserva LATB0
 #define EV_RECIRCULACAO   LATB1
 #define EV_ESGOTO         LATB2
 #define EV_REDE           LATB3
 #define BOMBA_AGUA        LATB4
 #define RESISTENCIA_AQUEC LATB5
-//#define OCUPADO LATB6
-//#define OCUPADO LATB7
+#define NVL_BAIXO   PORTAbits.RA2
+#define NVL_ALTO    PORTAbits.RA1
 
 //#pragma config CONFIG1H = 0x22
 __CONFIG(1, FOSC_HSHP & PLLCFG_OFF & PRICLKEN_ON & FCMEN_OFF & IESO_OFF);
@@ -77,33 +76,52 @@ __CONFIG(11, EBTRB_OFF);
 
 char RECEIVED_COMMAND = 0;
 
+
 void interrupt ISR() {
     if (RC1IF && RC1IE) {
-        // Interrupcao serie
         RECEIVED_COMMAND = RCREG1;
-    }
+    } 
 }
-int adc_capture(){
+
+void EEPROM_write(uint8_t adress, uint8_t value){
     
-    ADCON0bits.GO = 1;
-    while (ADCON0bits.GO == 1);
+    EECON1bits.EEPGD = 0; //Data EEPROM
+    EECON1bits.CFGS  = 0; //Acess data EEPROM
+    EECON1bits.WREN  = 1; // Write Enable
     
-    uint16_t code = 0x0000;         //16bit var reset
-    code = ADRESH;                  //High byte -> 0b00000000 000000HH;
-    code = code << 8;               //Shift left 8 times  -> 0b000000HH 00000000
-    code = code | ADRESL;           // 0b000000HH LLLLLLLL 
+    EEADR = adress;
+    EEDATA = value;
+    
+    INTCONbits.GIE = 0;   // Disable Interrupts
+    /*Write Sequence*/
+    EECON2 = 0x55;
+    EECON2 = 0xAA;
+    EECON1bits.WR = 1;
+    /*End Write Sequence*/
+    INTCONbits.GIE = 1;   // Enable Interrupts
+    
+    EECON1bits.WREN  =0; // Write Disable
+}
+
+uint8_t EEPROM_read(uint8_t adress){
+    
+    EEADR = adress;
+    EECON1bits.EEPGD = 0; //Data EEPROM
+    EECON1bits.CFGS  = 0; //Acess data EEPROM
+    RD = 1;
   
-    return code;
+    return EEDATA;
 }
+
 void PORT_init(){
     /* General IO*/
     TRISB = 0;
     LATB = 0;
-    
-    TRISA0 = 1; // Sensor de nivel (alto)
-    TRISA1 = 1; // Sensor de nivel (baixo)
-    ANSA0 = 0;
-    ANSA1 = 0;
+    ANSB1  = 0;
+    ANSB2  = 0;
+    ANSB3  = 0;
+    ANSB4  = 0;
+    ANSB5  = 0;
     
     /*UART IO*/
     TRISC6 = 0;             // OUTPUT TX
@@ -112,13 +130,20 @@ void PORT_init(){
  
     /*ADC IO*/
     ANSELA = 0;
-    TRISA1 = 1; 
-    ANSA1  = 1;
+    TRISA0 = 1; 
+    ANSA0  = 1;
+    
+    /*Entradas digitais*/
+    TRISA1 = 1; 	// Input NVL_BAIXO
+    TRISA2 = 1; 	//Input NVL_ALTO
+    ANSA1  = 0; 	//Input DIGITAL
+    ANSA2  = 0; 	//Input DIGITAL
     
     /*Interrupts*/
     PEIE = 1;
-    GIE = 1;
+    GIE  = 1;
 }
+
 void UART_init(){
     /*Baud Rate*/
     SPBRGH1 = 0;
@@ -132,11 +157,10 @@ void UART_init(){
     TXSTA1bits.TXEN    = 1; 
     RCSTA1bits.CREN    = 1;
     
-    RC1IE = 1; //Interrupt flag Enable
-
+    RC1IE = 1; 	//Interrupt flag Enable
 }
-void ADC_init(){
 
+void ADC_init(){
     /*Config Bits*/
     ADCON0bits.CHS   = 0b00000;        //Analog channel AN1 
     ADCON1bits.PVCFG = 0b00;           //01 A/D VREF+ connected to external pin, VREF+
@@ -145,12 +169,25 @@ void ADC_init(){
     ADCON2bits.ACQT  = 0b010;          // 20 Tad                   
     ADCON2bits.ADCS  = 0b010;          // Fosc/32                  
     ADCON0bits.ADON  = 1;              // ADC ON
-    
 }
+
+uint16_t adc_capture(){    
+    ADCON0bits.GO = 1;
+    while (ADCON0bits.GO == 1);
+    
+    uint16_t code = 0x0000;         //16bit var reset
+    code = ADRESH;                  //High byte -> 0b00000000 000000HH;
+    code = code << 8;               //Shift left 8 times  -> 0b000000HH 00000000
+    code = code | ADRESL;           // 0b000000HH LLLLLLLL 
+  
+    return code;
+}
+
 float Temp_calc(float Rterm){                                      //Calculate Temperature based on Thermistor resistor value;
     float t = (pow((1./(float)T0)+(1./(float)BETA)*log((float)Rterm/(float)R0),(-1.)))-273.16;
     return t;
 }
+
 float Rterm_calc(char method, float code){     
     float R;
     if (method == 'A'){
@@ -161,6 +198,7 @@ float Rterm_calc(char method, float code){
     }
     return R;
 }
+
 float adc_average(int samples){
     float average = 0;
     uint8_t i;
@@ -172,83 +210,199 @@ float adc_average(int samples){
     
     return average;
 }
+
 void main() {
     PORT_init();
     UART_init();
     ADC_init();
     
-    // **** CURRENT FISH CONFIG ****
+    /* Fish Parameters Load */
     FishParams current_fish;
-    set_current_fish(&current_fish, 2);
-
-    char machine_state = STATE_STOPPED;   // Define initial machine state
-    //set_machine_state(machine_state);     // Sets initial machine state
+    if (EEPROM_read(0x00) != 255)
+        set_current_fish(&current_fish, EEPROM_read(0x00));
+    else
+        set_current_fish(&current_fish, 1);
     
     float adc_capture_average;
     float Rterm;
     float Temp_measured;
     
+    uint8_t last_fsm_state = 0;
+    //uint8_t fsm_state    = 0;
+    uint8_t fsm_state      = EEPROM_read(0x10);
+    if (fsm_state != 1)
+        fsm_state = 0;
+    
+    int timer_preload = 55770;
+    int t1_counter    = 0;
+    
+    uint8_t last_NVL_ALTO = 0;
+    /* TIMER CONFIG - p.154 */
+    T0CONbits.TMR0ON = 1;
+    T0CONbits.T08BIT = 0; // Usar timer 16 bits
+    T0CONbits.T0CS   = 0; // Usa o mesmo clock que o microncontrolador
+    T0CONbits.PSA    = 0; // Ativa Prescaler
+    T0CONbits.T0PS   = 0b111; // Prescaler 128  
+    TMR0H = (timer_preload >> 8 & 0xFF);
+    TMR0L = (timer_preload & 0xFF);
+    
+    /* TIMER 1 CONFIG */
+    T1CONbits.TMR1CS  = 0b00; 
+    T1CONbits.T1CKPS  = 0b11; //1:8 pré (máx)
+    T1CONbits.T1RD16  = 1;
+    T1CONbits.TMR1ON  = 0;
+    
     while (1){
-            
-        /* ---------------- Temperature control ---------------- */
-        adc_capture_average = adc_average(200);         //Captura e faz a média de 200 amostras do ADC;
-        Rterm = Rterm_calc('B', adc_capture_average);   //Converte o codigo do adc em resistência
-        Temp_measured = Temp_calc(Rterm);               //Converte o valor de resistência em Temperatura
-                
-        if (Temp_measured <= (current_fish.average_temperature - 2)){
-            RESISTENCIA_AQUEC = 1;
-        }
-        else if(Temp_measured < current_fish.average_temperature){
-            asm("nop");
-        }
-        else{
-            RESISTENCIA_AQUEC = 0;
-        }
         
-        /* ---------------- Command processing ---------------- */
+        /* ---------------- Temperature Measurement ---------------- */
+        adc_capture_average = adc_average(200);         //Captura e faz a média de 200 amostras do ADC;
+        Rterm = Rterm_calc('A', adc_capture_average);   //Converte o codigo do adc em resistência
+        Temp_measured = Temp_calc(Rterm);               //Converte o valor de resistência em Temperatura
+        
+        
         if (RECEIVED_COMMAND) {
             switch (RECEIVED_COMMAND) {
-                /* Machine State commands */
-                case COMMAND_START_MACHINE:
-                    machine_state = STATE_CIRCULATION;
-                    break;
-                case COMMAND_STOP_MACHINE:
-                    machine_state = STATE_STOPPED;
-                    break;
-                case COMMAND_START_DRAINNING:
-                    machine_state = STATE_DRAINNING;
-                    break;
-                case COMMAND_START_FILLING:                  
-                    machine_state = STATE_FILLING;
-                    break;
-                
                 /* Fish change commands */
                 case COMMAND_CHANGE_FISH_1:
                     set_current_fish(&current_fish, 6);
+                    EEPROM_write(0,'1');
                     break;
                 case COMMAND_CHANGE_FISH_2:
                     set_current_fish(&current_fish, 2);
+                    EEPROM_write(0,'2');
                     break;
                 case COMMAND_CHANGE_FISH_3:
                     set_current_fish(&current_fish, 1);
+                    EEPROM_write(0,'3');
                     break;
                 case COMMAND_CHANGE_FISH_4:
                     set_current_fish(&current_fish, 5);
+                    EEPROM_write(0,'4');
                     break;
                 case COMMAND_CHANGE_FISH_5:
                     set_current_fish(&current_fish, 4);
+                    EEPROM_write(0,'5');
                     break;
                 case COMMAND_CHANGE_FISH_6:
                     set_current_fish(&current_fish, 3);
+                    EEPROM_write(0,'6');
                     break;
-            }
-            RECEIVED_COMMAND = 0;
+            }                       
         }
-
-        send_sys_data(machine_state, current_fish.identifier, Temp_measured);
-            
         
-        //__delay_ms(25);
+        /* FSM main */
+        switch(fsm_state){
+            case 0:                      //Estado inicial / neutro
+                EV_RECIRCULACAO   = 0;
+                EV_ESGOTO         = 0;
+                EV_REDE           = 0;
+                BOMBA_AGUA        = 0;
+                RESISTENCIA_AQUEC = 0;
+                if (RECEIVED_COMMAND == COMMAND_START_MACHINE){
+                    fsm_state = 1;
+                }
+                if (RECEIVED_COMMAND == COMMAND_START_DRAINNING){
+                    fsm_state = 2;
+                }
+                if ((RECEIVED_COMMAND == COMMAND_START_FILLING) && !NVL_ALTO){
+                    fsm_state = 3;
+                }    
+                break;
+            
+            case 1: //Recirculação
+                EV_RECIRCULACAO   = 1;
+                EV_ESGOTO         = 0;
+                EV_REDE           = 0;
+                BOMBA_AGUA        = 1;
+                RESISTENCIA_AQUEC = 0;
+                
+                if ((Temp_measured <= (current_fish.average_temperature - 2)) && NVL_ALTO){                 
+                        RESISTENCIA_AQUEC = 1;
+                }
+                else if(Temp_measured < current_fish.average_temperature){
+                    asm("nop");
+                }
+                else{
+                    RESISTENCIA_AQUEC = 0;
+                }                
+                
+                if (RECEIVED_COMMAND == COMMAND_STOP_MACHINE || !NVL_BAIXO){
+                    fsm_state = 0;
+                } else if (RECEIVED_COMMAND == COMMAND_START_FILLING && !NVL_ALTO){
+                    fsm_state = 3;
+                } else if (RECEIVED_COMMAND == COMMAND_START_DRAINNING && NVL_BAIXO){
+                    fsm_state = 2;
+                }
+                
+                break;
+            
+            case 2: //Vazamento
+                EV_RECIRCULACAO = 0;
+                EV_ESGOTO = 1;
+                EV_REDE = 0;
+                BOMBA_AGUA = 1;
+                RESISTENCIA_AQUEC = 0;
+                
+                if (RECEIVED_COMMAND == COMMAND_STOP_MACHINE || !NVL_BAIXO){
+                    fsm_state = 0;
+                } else if (RECEIVED_COMMAND == COMMAND_START_FILLING && !NVL_ALTO){
+                    fsm_state = 3;
+                } else if (RECEIVED_COMMAND == COMMAND_START_MACHINE && NVL_BAIXO){
+                    fsm_state = 1;
+                }
+                break;
+            
+            case 3: //Enchimento
+                EV_RECIRCULACAO   = 0;
+                EV_ESGOTO         = 0;
+                EV_REDE           = 1;
+                BOMBA_AGUA        = 0;
+                RESISTENCIA_AQUEC = 0;
+                if (NVL_ALTO){
+                    T1CONbits.TMR1ON  = 1;                
+                }        
+                
+                // Temporização Overflow
+                if (TMR1IF){                    
+                    if (t1_counter > 300){
+                        T1CONbits.TMR1ON  = 0;
+                        t1_counter = 0;
+                    fsm_state = 0;
+                    } else {
+                        t1_counter ++;
+                    }
+                    
+                    TMR1H = (timer_preload >> 8 & 0xFF);
+                    TMR1L = (timer_preload & 0xFF);
+                    TMR1IF = 0;
+                }
+                                
+                if (RECEIVED_COMMAND == COMMAND_STOP_MACHINE){
+                    fsm_state = 0;
+                    T1CONbits.TMR1ON  = 0;
+                } else if (RECEIVED_COMMAND == COMMAND_START_DRAINNING && NVL_BAIXO){
+                    fsm_state = 2;
+                } else if (RECEIVED_COMMAND == COMMAND_START_MACHINE && NVL_BAIXO){
+                    fsm_state = 1;
+                }
+                break;
+
+        }
+        
+        /* Update EEPROM Value */     
+        if (fsm_state != last_fsm_state){ 	//Update EEPROM when fsm_state changes
+            EEPROM_write(0x10,fsm_state);        
+        }
+        last_fsm_state = fsm_state;
+        
+        /* Serial Send */
+        if (TMR0IF) {
+            send_sys_data(fsm_state, current_fish.identifier, Temp_measured);
+            TMR0IF = 0;
+            TMR0H = (timer_preload >> 8 & 0xFF);
+            TMR0L = (timer_preload & 0xFF);
+        }
+        RECEIVED_COMMAND = 0;
     }
 }
 
